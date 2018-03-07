6382354f93366a408ffb72d9d9cbfd2a03ca5877
Joshua Lan <josh.lan9@gmail.com>
2018-03-07 10:51:47 -0500

    Use feedback to query for better results form Cloudant

--- actions/feedback-service.js
+++ actions/feedback-service.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const NLCV1 = require('watson-developer-cloud/natural-language-classifier/v1');
+const Cloudant = require('@cloudant/cloudant')
 
 function main(params) {
   return new Promise((resolve, reject) => {
@@ -10,6 +11,10 @@ function main(params) {
     assert(params.classifier_id, 'params.classifier_id cannot be null');
 
     assert(params.feedback, 'params.feedback cannot be null');
+
+    assert(params.cloudantUsername, 'params.cloudantUsername cannot be null')
+    assert(params.cloudantPassword, 'params.cloudantPassword cannot be null')
+    assert(params.dbName, 'params.dbName cannot be null')
 
     const classifier = new NLCV1({
       username: params.nlcUsername,
@@ -24,6 +29,31 @@ function main(params) {
         reject(err)
       }
       resolve(data)
+    })
+  }).then(result => {
+    return new Promise((resolve, reject) => {
+      const cloudant = new Cloudant({
+        account: params.cloudantUsername,
+        password: params.cloudantPassword
+      });
+      const database = cloudant.db.use(params.dbName);
+      if (result.top_class === "PRICEREDUCE") {
+        database.find({selector:{"rpi":{"$lt":params.min_rpi}}}, (er, result) => {
+          if (er) {
+            reject(er)
+          }
+          resolve(result)
+        })
+      } else if (result.top_class === "PRICEINCREASE") {
+        database.find({selector:{"rpi":{"$gt":params.max_rpi}}}, (er, result) => {
+          if (er) {
+            reject(er)
+          }
+          resolve(result)
+        })
+      } else {
+        //TODO NLC always returns a top_class, need to check for garbage inputs.
+      }
     })
   })
 }
--- create-actions.sh
+++ create-actions.sh
@@ -14,11 +14,14 @@ bx wsk action update $PACKAGE/feedback-service \
   --param nlcUsername $NLC_USERNAME \
   --param nlcPassword $NLC_PASSWORD \
   --param classifier_id $CLASSIFIER_ID \
+  --param cloudantUsername $CLOUDANT_USERNAME \
+  --param cloudantPassword $CLOUDANT_PASSWORD \
+  --param dbName $DB_NAME
 
 echo "Retrieving Action URL..."
 API_URL=`bx wsk action get $PACKAGE/feedback-service --url | sed -n '2p'`;
 API_URL+=".json"
 
 # Write API Url to .env file
-head -n 4 .env | cat >> .env_tmp; mv .env_tmp .env
+head -n 10 .env | cat >> .env_tmp; mv .env_tmp .env
 echo "REACT_APP_API_URL=$API_URL" >> .env
